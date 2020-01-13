72b9dd87980d914239eecde5a27d6a41f1cf0478
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-01-13 13:51:26 -0800

    made a game

--- App.tsx
+++ App.tsx
@@ -6,6 +6,30 @@ function Cell(props: { state: string, clickHandler: () => void }) {
     return <Text onPress={props.clickHandler}>{props.state}</Text>;
 }
 
+//check diagonals, and along col and row
+function isWin(state: Array<Array<string>>): boolean {
+    for (let i = 0; i < 3; i++) {
+        if ((state[i][0] === state[i][1] && state[i][1] === state[i][2] && state[i][0] !== '_') ||
+            (state[0][i] === state[1][i] && state[1][i] === state[2][i] && state[0][i] !== '_')) {
+            return true;
+        }
+    }
+    return ((state[0][0] === state[1][1] && state[1][1] === state[2][2]) || //cross
+        (state[2][0] === state[1][1] && state[1][1] === state[0][2])) && state[1][1] !== '_';
+}
+
+//all spaces are set is a tie
+function isTie(state: Array<Array<string>>): boolean {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (state[i][j] === '_') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 function reducer(state: Array<Array<string>>, action: { row: number, col: number }): Array<Array<string>> {
     console.log("rc target: ", action.row, action.col);
     switch (state[action.row][action.col]) {
@@ -35,9 +59,8 @@ export default function App() {
                                                         clickHandler={() => dispatch({row: r, col: c})}
                                                         key={c}/>)
                     }
-                </View>
-            )
-            }
+                </View>)}
+            {isWin(board) ? <Text>"you win!"</Text> : (isTie(board) && <Text>"a tie!"</Text>)}
         </View>
     );
 }
