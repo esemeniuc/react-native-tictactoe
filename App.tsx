c07cad64cdb98f13b66ee0c4443867f7c700771d
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-01-13 12:39:53 -0800

    tiny tic tac toe

--- App.tsx
+++ App.tsx
@@ -1,51 +1,58 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {range} from 'lodash';
+import {chunk} from 'lodash';
 
-function Cell(props: { a: string }) {
-    return <div>{props.a}</div>;
+function Cell(props: { state: string, clickHandler: () => void }) {
+    return <>
+        <Text onPress={props.clickHandler}>{props.state}</Text>
+    </>
+}
+
+function reducer(state: Array<Array<string>>, action: { row: number, col: number }): Array<Array<string>> {
+    console.log("rc target: ", action.row, action.col);
+    console.log("old: ", state);
+
+    switch (state[action.row][action.col]) {
+        case 'x':
+            state[action.row][action.col] = 'o';
+            console.log("new: ", state);
+            return [...state];
+        case 'o':
+            state[action.row][action.col] = '_';
+            console.log("new: ", state);
+            return [...state];
+        case '_':
+            state[action.row][action.col] = 'x';
+            console.log("new: ", state);
+            return [...state];
+        default:
+            throw new Error();
+    }
 }
 
 export default function App() {
-    console.log(styles);
-    const [board, setBoard] = useState<Array<Array<string>>>(Array(3).fill(Array(3).fill('x')));
+    const [board, dispatch] = useReducer(reducer, chunk(Array(9).fill('_'), 3));
     return (
         <View style={styles.container}>
             <Text>Open up App.tsx to start working on your app!</Text>
-            <table>
-                <tbody>
-                {board.map(row =>
-                    <tr>
-                        {row.map(stateChar => <td><Cell a={stateChar}/></td>)}
-                    </tr>)
-                }
-                </tbody>
-            </table>
+            {board.map((row, r) =>
+                <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-evenly'}} key={r}>
+                    {
+                        row.map((stateChar, c) => <Cell state={stateChar}
+                                                        clickHandler={() => dispatch({row: r, col: c})}
+                                                        key={c}/>)
+                    }
+                </View>
+            )
+            }
         </View>
     );
 }
 
-// const styles2 = StyleSheet.create({
-//     div: {
-//         backgroundColor: '#fff',
-//
-//         // borderWidth: 1,
-//         // borderColor: 'black',
-//         // borderStyle: 'solid',
-//     },
-// });
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    td: {
-        backgroundColor: '#fff',
-        //
-        //     // borderWidth: 1,
-        //     // borderColor: 'black',
-        //     // borderStyle: 'solid',
-    },
+        // alignItems: 'stretch',
+        justifyContent: 'space-evenly'
+    }
 });
